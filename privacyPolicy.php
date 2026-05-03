5e11c5f818faa767bde482dcfb748fe6493926a2
Faisal-9 <afsahibzada16@gmail.com>
2026-05-03 15:01:29 +0430

    header top has been changed

---
+++ privacyPolicy.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<?php
+$page_title = "Contact Us - State Corps";
+include("includes/head.php");
+
+
+
+
+
+?>
+
+<body>
+
+    <?php include("includes/header.php"); ?>
+
+    <main>
+        <section class="container">
+
+        </section>
+    </main>
+
+    <?php include("includes/footer.php"); ?>
+
+    <?php include("includes/footerLink.php"); ?>
+
+</body>
+
+</html>
