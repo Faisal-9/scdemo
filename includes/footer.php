e10983caa7d28fba2222cb7ca050e4972a1dd4f3
Faisal-9 <afsahibzada16@gmail.com>
2026-06-08 11:22:59 +0430

    footer design and contents changed and updated

--- includes/footer.php
+++ includes/footer.php
@@ -1,78 +1,169 @@
-<?php $current_year = date("Y"); ?>
+<footer class="site-footer">
 
-<footer class="site-footer py-3">
     <div class="container">
 
-        <!-- TOP SECTION -->
-        <div class="footer-top row g-3 align-items-start">
+        <!-- TOP FOOTER -->
+        <div class="footer-main">
 
-            <!-- LEFT -->
-            <div class="col-lg-5 col-12 footer-left text-center text-lg-start">
-                <p>STATE CORPS, Building Afghanistan's Future</p>
-                <p class="footer-tagline">
-                    ISO 18001:2007 Certified
-                </p>
-                <p class="footer-tagline">
-                    ISO 14001:2015 Certified
-                </p>
-                <p class="footer-tagline">
-                    ISO 9001:2015 Certified
-                </p>
+            <!-- COMPANY -->
+            <div class="footer-col footer-company">
 
-                <!-- <div class="social-icons mt-3 justify-content-center justify-content-lg-start">
-                    <a href="https://www.facebook.com/StateCorpsInc/" target="_blank" aria-label="Facebook">
-                        <i class="fa-brands fa-facebook-f"></i>
-                    </a>
-                    <a href="https://twitter.com/StateCorps" target="_blank" aria-label="Twitter">
-                        <i class="fa-brands fa-x-twitter"></i>
-                    </a>
-                    <a href="https://www.linkedin.com/company/state-corps" target="_blank" aria-label="LinkedIn">
-                        <i class="fa-brands fa-linkedin-in"></i>
-                    </a>
-                </div> -->
+                <h2 class="footer-name">State Corps</h2>
+
+                <div class="footer-statement">
+                    Building Infrastructure.
+                    Empowering Communities.
+                    Driving Sustainable Growth.
+                </div>
+
             </div>
 
-            <!-- RIGHT -->
-            <div class="col-lg-7 col-12 footer-right">
+            <!-- SERVICES -->
+            <div class="footer-col">
 
-                <div class="footer-links">
-                    <ul>
-                        <li><a href="services.php?tab=engineeringanddesign#engineeringanddesign-engineering-services">Engineering services</a></li>
-                        <li><a href="services.php?tab=engineeringanddesign#engineeringanddesign-design-services">Design services</a></li>
-                        <li><a href="services.php?tab=mining">Mining Services</a></li>
-                        <li><a href="services.php?tab=implementation#implementation-project-management">Project Management</a></li>
-                        <li><a href="sectors.php?tab=powerenergy">Power &amp; Energy sector</a></li>
-                        <li><a href="publicv6/sectors.php?tab=transport">Transport sector</a></li> 
-                        <li><a href="publicv6/sectors.php?tab=Infrastructure">Transport sector</a></li> 
-                        <li><a href="sectors.php?tab=mining">Mining sector</a></li>
-                        <li><a href="projects.php">Our Projects</a></li>
-                        <li><a href="about.php#general-info">Company Overview</a></li>
-                        <li><a href="about.php#mission-vision">Our Mission & Vision</a></li>
-                        <li><a href="about.php#clients">Our Clients</a></li>
-                    </ul>
+                <h4>Services</h4>
+
+                <ul>
+                    <li>
+                        <a href="services.php?tab=engineeringanddesign#engineeringanddesign-engineering-services">
+                            Engineering Services
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="services.php?tab=engineeringanddesign#engineeringanddesign-design-services">
+                            Design Services
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="services.php?tab=mining">
+                            Mining Services
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="services.php?tab=implementation#implementation-project-management">
+                            Project Management
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="services.php?tab=implementation">
+                            EPC Solutions
+                        </a>
+                    </li>
+                </ul>
+
+            </div>
+
+            <!-- COMPANY -->
+            <div class="footer-col">
+
+                <h4>Company</h4>
+
+                <ul>
+
+                    <li>
+                        <a href="about.php#general-info">
+                            Company Overview
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="about.php#mission-vision">
+                            Mission & Vision
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="projects.php">
+                            Projects
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="media.php">
+                            News & Media
+                        </a>
+                    </li>
+
+                    <li>
+                        <a href="contact.php">
+                            Contact Us
+                        </a>
+                    </li>
+
+                </ul>
+
+            </div>
+
+            <!-- CONTACT -->
+            <div class="footer-col">
+
+                <h4>Contact</h4>
+
+                <ul class="footer-contact">
+
+                    <li>
+                        <i class="fa-solid fa-location-dot"></i>kart-e-char Kabul, Afghanistan
+                    </li>
+
+                    <li>
+                        <i class="fa-solid fa-phone"></i>+93 791 811 968
+                    </li>
+
+                    <li>
+                        <i class="fa-solid fa-envelope"></i>comms@statecorps.com
+                    </li>
+
+                </ul>
+
+                <div class="footer-social">
+
+                    <a href="#">
+                        <i class="fab fa-linkedin-in"></i>
+                    </a>
+
+                    <a href="#">
+                        <i class="fab fa-facebook-f"></i>
+                    </a>
+
+                    <a href="#">
+                        <i class="fab fa-x-twitter"></i>
+                    </a>
+
                 </div>
 
             </div>
 
         </div>
 
-        <!-- DIVIDER -->
-        <div class="footer-divider"></div>
-
-        <!-- BOTTOM -->
+        <!-- FOOTER BOTTOM -->
         <div class="footer-bottom">
-            <div class="footer-bottom-left">
-                <p>© <?php echo $current_year ?> State Corps. All rights reserved</p>
+
+            <div class="footer-copyright">
+                © 2026 State Corps. All Rights Reserved.
             </div>
 
-            <div class="footer-bottom-right">
-                <a href="#">Trademarks</a>
-                <span>|</span>
-                <a href="privacypolicy.php">Privacy Policy</a>
-                <span>|</span>
-                <a href="terms.php">Terms of Service</a>
+            <div class="footer-policy">
+
+                <a href="privacypolicy.php">
+                    Privacy Policy
+                </a>
+
+                <a href="terms.php">
+                    Terms of Service
+                </a>
+
+                <a href="#">
+                    Trademarks
+                </a>
+
             </div>
+
         </div>
 
     </div>
+
 </footer>
